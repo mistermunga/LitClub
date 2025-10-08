2d62eabb9379b349a7767e41e6d6d97ad09a6599
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-08 11:49:30 +0300

    defined method for showing Landing Page

--- src/main/java/com/litclub/MainApplication.java
+++ src/main/java/com/litclub/MainApplication.java
@@ -1,18 +1,34 @@
 package com.litclub;
 
+import com.litclub.ui.LandingPage;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
 
-    @Override
-    public void start(Stage stage) {
-        stage.setScene(new Scene(new Label("LitClub Launcher!"), 300, 200));
-        stage.setTitle("LitClub");
-        stage.show();
+    private static MainApplication instance;
+    private Stage primaryStage = new Stage();
+
+    public static MainApplication getInstance() {
+        return instance;
     }
+
+    @Override
+    public void start(Stage primaryStage) {
+        instance = this;
+        primaryStage.setTitle("LitClub Desktop");
+        showLanding();
+    }
+
+    public void showLanding() {
+        LandingPage landingPage = new LandingPage();
+        Scene scene = new Scene(landingPage);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public void showLogin() {}
 
     public static void main(String[] args) {
         launch(args);
