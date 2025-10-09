701371f44b567c10074b0d7bde5533b37d2e6cec
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-09 20:59:57 +0300

    subcomponents for the App header

---
+++ src/main/java/com/litclub/ui/component/subcomponent/MiniLogo.java
@@ -0,0 +1,41 @@
+package com.litclub.ui.component.subcomponent;
+
+import com.litclub.theme.ThemeManager;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.util.Objects;
+
+public class MiniLogo extends ImageView {
+
+    public MiniLogo() {
+        String imagePath = ThemeManager.getInstance().isBrightMode()
+                ? "/com/litclub/ui/icons/LitClub_logo_blackText_mini.png"
+                : "/com/litclub/ui/icons/LitClub_logo_whiteText_mini.png";
+
+        Image image = new Image(
+                Objects.requireNonNull(getClass().getResourceAsStream(imagePath)
+                , "Mini logo not found at" + imagePath)
+        );
+
+        this.setImage(image);
+        this.setPreserveRatio(true);
+        this.setFitWidth(35);
+        this.getStyleClass().add("page-logo");
+
+        ThemeManager.getInstance().brightModeProperty().addListener(
+                (_, _, newValue) -> {
+                    String newPath = newValue
+                            ? "/com/litclub/ui/icons/LitClub_logo_blackText_mini.png"
+                            : "/com/litclub/ui/icons/LitClub_logo_whiteText_mini.png";
+
+                    Image newImage = new Image(
+                            Objects.requireNonNull(getClass().getResourceAsStream(newPath)
+                                    , "Mini logo not found at" + newPath)
+                    );
+
+                    this.setImage(newImage);
+                }
+        );
+    }
+}
