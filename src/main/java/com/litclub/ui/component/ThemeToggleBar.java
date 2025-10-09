fab020c179f9962081fcf2b27f2781402bfe5509
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-09 14:09:27 +0300

    extracted the button for changing themes into it's own class

---
+++ src/main/java/com/litclub/ui/component/ThemeToggleBar.java
@@ -0,0 +1,36 @@
+package com.litclub.ui.component;
+
+import com.litclub.theme.ThemeManager;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+public class ThemeToggleBar extends VBox {
+
+    public ThemeToggleBar() {
+        this.setAlignment(Pos.CENTER);
+        this.setPadding(new Insets(20, 0, 0, 0));
+
+        showToggleButton();
+    }
+
+    private void showToggleButton() {
+        // Theme toggle button
+        Button toggleButton = new Button();
+        toggleButton.getStyleClass().add("theme-toggle");
+        updateToggleButtonText(ThemeManager.getInstance().isBrightMode(), toggleButton);
+
+        // Update button text on theme change
+        ThemeManager.getInstance().brightModeProperty().addListener(
+                (observable, oldValue, newValue) -> updateToggleButtonText(newValue, toggleButton)
+        );
+        toggleButton.setOnAction(event -> ThemeManager.getInstance().toggleTheme());
+
+        this.getChildren().add(toggleButton);
+    }
+
+    private void updateToggleButtonText(boolean isBrightMode, Button button) {
+        button.setText(isBrightMode ? "Switch to Dark Mode" : "Switch to Light Mode");
+    }
+}
