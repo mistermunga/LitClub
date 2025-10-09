d524fa770e51063418818baecf9f7d271c6fb79f
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-09 16:40:33 +0300

    implemented new records, and streamlined the class

--- src/main/java/com/litclub/session/AppSession.java
+++ src/main/java/com/litclub/session/AppSession.java
@@ -1,16 +1,13 @@
 package com.litclub.session;
 
-import java.util.HashMap;
+import com.litclub.session.construct.ClubRecord;
+import com.litclub.session.construct.UserRecord;
 
 public class AppSession {
 
     private static AppSession instance;
-
-    private String username;
-    private String fName;
-    private String lName;
-    private String email;
-    private String club;
+    private UserRecord userRecord;
+    private ClubRecord clubRecord;
     private boolean brightMode = true;
 
     private AppSession() {}
@@ -22,22 +19,6 @@ public class AppSession {
         return instance;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public String getfName() {
-        return fName;
-    }
-
-    public String getlName() {
-        return lName;
-    }
-
-    public String getEmail() { return email; }
-
-    public String getClub() { return club; }
-
     public boolean isBrightMode() {
         return brightMode;
     }
@@ -46,40 +27,19 @@ public class AppSession {
         this.brightMode = brightMode;
     }
 
-    public void setlName(String lName) {
-        this.lName = lName;
+    public void setCredentials(
+            String firstname,
+            String lastname,
+            String username,
+            String email
+    ) {
+        this.userRecord = new UserRecord(firstname, lastname, username, email);
     }
 
-    public void setfName(String fName) {
-        this.fName = fName;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) { this.email = email; }
-
-    public void setClub(String club) { this.club = club; }
-
-    public void setCredentials(String firstname,
-                               String lastname,
-                               String username,
-                               String email) {
-        setfName(firstname);
-        setlName(lastname);
-        setUsername(username);
-        setEmail(email);
-    }
-
-    public HashMap<String,String> getCredentials() {
-        HashMap<String,String> credentials = new HashMap<>();
-
-        credentials.put("firstname", getfName());
-        credentials.put("lastname", getlName());
-        credentials.put("username", getUsername());
-        credentials.put("email", getEmail());
-
-        return credentials;
+    public void setClubDetails(
+            String clubName,
+            boolean isAdministrator
+    ) {
+        this.clubRecord = new ClubRecord(clubName, isAdministrator);
     }
 }
