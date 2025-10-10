a9caf4fc3bce046aa92329a4bdd7a77d8f78b525
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-10 22:36:35 +0300

    refactored application to use the Scene manager

--- src/main/java/com/litclub/MainApplication.java
+++ src/main/java/com/litclub/MainApplication.java
@@ -1,17 +1,8 @@
 package com.litclub;
 
-import com.litclub.session.AppSession;
-import com.litclub.session.construct.ClubRecord;
-import com.litclub.theme.ThemeManager;
-import com.litclub.ui.ClubPage;
-import com.litclub.ui.CrossRoadsPage;
-import com.litclub.ui.LandingPage;
-import com.litclub.ui.LoginPage;
 import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
-import java.util.List;
 
 public class MainApplication extends Application {
 
@@ -26,56 +17,21 @@ public class MainApplication extends Application {
     public void start(Stage primaryStage) {
         instance = this;
         this.primaryStage = primaryStage;
-        primaryStage.setTitle("LitClub Desktop");
 
-        primaryStage.setHeight(600);
-        primaryStage.setWidth(550);
-        showLanding();
+        // One-time stage setup only
+        primaryStage.getIcons().add(
+                new Image(getClass().getResourceAsStream(
+                        "/com/litclub/ui/icons/LitClub_logo_blackText_mini.png"
+                ))
+        );
+
+        // Delegate to SceneManager for all navigation
+        SceneManager.getInstance().showLanding();
     }
 
-    public void showLanding() {
-        LandingPage landingPage = new LandingPage();
-        Scene scene = new Scene(landingPage);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
-
-    public void showLogin() {
-        ThemeManager.getInstance().clearRegisteredComponents();
-        LoginPage loginPage = new LoginPage();
-        Scene scene = new Scene(loginPage);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    // TODO Another API call, find all user clubs
-    public void showCrossRoads() {
-        List<String> clubs = List.of("Society of the Bard", "Gumshoe Sleuthers");
-        ThemeManager.getInstance().clearRegisteredComponents();
-        CrossRoadsPage crossRoadsPage = new CrossRoadsPage(clubs);
-        Scene scene = new Scene(crossRoadsPage);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    public void showClubPage(String clubName) {
-        ThemeManager.getInstance().clearRegisteredComponents();
-
-        // TODO [CRITICAL] API call, get club details
-        boolean adminPermission = clubName.equals("Society of the Bard");
-        ClubRecord cr = new ClubRecord(clubName, adminPermission);
-        AppSession.getInstance().setClubDetails(cr);
-
-        ClubPage clubPage = new ClubPage();
-        Scene scene = new Scene(clubPage);
-        primaryStage.setTitle(clubName);
-        primaryStage.setScene(scene);
-        primaryStage.setMaximized(true);
-        primaryStage.resizableProperty().setValue(false);
-        primaryStage.show();
-    }
-
-    public void showMainPage() {}
 
     public static void main(String[] args) {
         launch(args);
