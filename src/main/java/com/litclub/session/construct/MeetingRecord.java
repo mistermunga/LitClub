2c5463c087f9f754cb62e160357db804fd2e0b94
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-12 21:39:30 +0300

    created MeetingRecord

---
+++ src/main/java/com/litclub/session/construct/MeetingRecord.java
@@ -0,0 +1,14 @@
+package com.litclub.session.construct;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+public record MeetingRecord(
+        String meetingName,
+        LocalDateTime start,
+        LocalDateTime end,
+        ClubRecord club,
+        String location,
+        Optional<String> link
+) {
+}
