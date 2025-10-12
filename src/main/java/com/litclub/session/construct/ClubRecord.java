5e7f22ad635efa50a3a7fd7274464d1b402dd12b
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-12 21:38:21 +0300

    expanded the ClubRecord

--- src/main/java/com/litclub/session/construct/ClubRecord.java
+++ src/main/java/com/litclub/session/construct/ClubRecord.java
@@ -1,4 +1,10 @@
 package com.litclub.session.construct;
 
-public record ClubRecord(String name, boolean administrator){
+import java.util.Set;
+
+public record ClubRecord(String name,
+                         UserRecord administrator,
+                         Set<UserRecord> members)
+{
+
 }
