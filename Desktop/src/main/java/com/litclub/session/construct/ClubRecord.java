8fafd1fe627a0bbfdc38a114e50b2134a3c98395
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-15 21:42:18 +0300

    added club id

--- Desktop/src/main/java/com/litclub/session/construct/ClubRecord.java
+++ Desktop/src/main/java/com/litclub/session/construct/ClubRecord.java
@@ -2,7 +2,8 @@ package com.litclub.session.construct;
 
 import java.util.Set;
 
-public record ClubRecord(String name,
+public record ClubRecord(int clubID,
+                         String name,
                          UserRecord administrator,
                          Set<UserRecord> members)
 {
