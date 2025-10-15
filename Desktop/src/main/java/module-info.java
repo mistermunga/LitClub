45e0a01f0dbc30fd05a2de380aef70b3d302ef96
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-15 21:40:45 +0300

    added gson dependency

--- Desktop/src/main/java/module-info.java
+++ Desktop/src/main/java/module-info.java
@@ -2,8 +2,13 @@ module com.litclub {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires java.desktop;
+    requires com.google.gson;
+    requires com.fasterxml.jackson.databind;
 
-    opens com.litclub to javafx.fxml;
+
+    opens com.litclub to javafx.fxml, com.google.gson, com.fasterxml.jackson.databind;
+    opens com.litclub.persistence to com.google.gson;
 
     exports com.litclub;
 }
