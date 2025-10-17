2d5f3f48a944551d4eebdb98aa06cd2e72afae1a
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-17 21:24:39 +0300

    added them to the NotesView

--- Desktop/src/main/java/com/litclub/ui/component/content/NotesView.java
+++ Desktop/src/main/java/com/litclub/ui/component/content/NotesView.java
@@ -1,6 +1,25 @@
 package com.litclub.ui.component.content;
 
-import javafx.scene.layout.GridPane;
+import com.litclub.theme.ThemeManager;
+import com.litclub.ui.component.content.subcomponents.notes.NoteControlBar;
+import com.litclub.ui.component.content.subcomponents.notes.NotesCore;
+import javafx.scene.layout.BorderPane;
 
-public class NotesView extends GridPane {
+public class NotesView extends BorderPane {
+
+    public NotesView() {
+        ThemeManager.getInstance().registerComponent(this);
+        showControlBar();
+        showNotesCore();
+    }
+
+    public void showControlBar() {
+        NoteControlBar controlBar = new NoteControlBar();
+        this.setTop(controlBar);
+    }
+
+    public void showNotesCore() {
+        NotesCore notesCore = new NotesCore();
+        this.setCenter(notesCore);
+    }
 }
