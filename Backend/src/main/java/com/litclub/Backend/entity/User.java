2ae2e26d265fcc065478d757939af1bbcd7d2987
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-22 21:38:09 +0300

    created user entity

---
+++ Backend/src/main/java/com/litclub/Backend/entity/User.java
@@ -0,0 +1,50 @@
+package com.litclub.Backend.entity;
+
+import com.litclub.Backend.security.roles.GlobalRole;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long userID;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column
+    private String passwordHash;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "second_name")
+    private String secondName;
+
+    @ElementCollection(targetClass = GlobalRole.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_global_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<GlobalRole> globalRoles = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(
+            name = "created_at",
+            nullable = false,
+            updatable = false
+    )
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<ClubMembership> memberships = new HashSet<>();
+}
