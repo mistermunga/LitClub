96cd947fc2c4ec03f2a4eae22c5952fb9a05bd67
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-22 21:40:11 +0300

    began security config

---
+++ Backend/src/main/java/com/litclub/Backend/security/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.litclub.Backend.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfiguration {
+}
