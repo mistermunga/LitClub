1427a1f61b0442c1f78bb8b9c1a4ec4c66267d37
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-23 11:24:43 +0300

    created register class ID

---
+++ Backend/src/main/java/com/litclub/Backend/entity/compositeKey/RegisterID.java
@@ -0,0 +1,22 @@
+package com.litclub.Backend.entity.compositeKey;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Embeddable
+@EqualsAndHashCode
+@Getter @Setter
+public class RegisterID implements Serializable {
+
+    @Column(name = "meeting_id")
+    private Long meetingID;
+
+    @Column(name = "user_id")
+    private Long userID;
+
+}
