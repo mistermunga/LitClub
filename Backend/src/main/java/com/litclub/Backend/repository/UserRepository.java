55999cd2d25e5406750aff6959273ef19b1aa6f8
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-23 10:15:54 +0300

    unique query for spring security

---
+++ Backend/src/main/java/com/litclub/Backend/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.litclub.Backend.repository;
+
+import com.litclub.Backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("""
+        select distinct u
+        from User u
+        left join fetch u.globalRoles gr
+        left join fetch u.memberships m
+        left join fetch m.club c
+        left join fetch m.roles r
+        where u.username = :username
+        """)
+    Optional<User> findByUsernameWithMembershipsAndRoles(@Param("username") String username);
+}
+
