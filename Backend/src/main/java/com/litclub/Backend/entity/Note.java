0c69362864c0695f3d6d8fc0d1474302240127a7
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-23 11:25:10 +0300

    created Note entity

---
+++ Backend/src/main/java/com/litclub/Backend/entity/Note.java
@@ -0,0 +1,55 @@
+package com.litclub.Backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "notes")
+@Inheritance(strategy = InheritanceType.JOINED)
+@Getter @Setter
+public class Note {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long noteID;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "club_id")
+    private Club club;
+
+    @ManyToOne
+    @JoinColumn(name = "discussion_id")
+    private DiscussionPrompt discussionPrompt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column
+    private boolean isPrivate;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        if (isPrivate) {
+            this.club = null;
+            this.discussionPrompt = null;
+        }
+    }
+
+}
