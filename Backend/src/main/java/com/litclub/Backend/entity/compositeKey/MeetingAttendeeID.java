358e10e75743b209c542669bb2a568aae9d46dc9
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-23 10:19:48 +0300

    added missing annotations

--- Backend/src/main/java/com/litclub/Backend/entity/compositeKey/MeetingAttendeeID.java
+++ Backend/src/main/java/com/litclub/Backend/entity/compositeKey/MeetingAttendeeID.java
@@ -2,10 +2,15 @@ package com.litclub.Backend.entity.compositeKey;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Embeddable
+@EqualsAndHashCode
+@Getter @Setter
 public class MeetingAttendeeID implements Serializable {
 
     @Column(name = "meeting_id")
