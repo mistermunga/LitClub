f9dcff33b32567c5eaa4a3d896c521eb496a9b0e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-28 00:24:15 +0300

    relevant queries

--- Backend/src/main/java/com/litclub/Backend/repository/ClubMembershipRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/ClubMembershipRepository.java
@@ -1,9 +1,19 @@
 package com.litclub.Backend.repository;
 
+import com.litclub.Backend.entity.Club;
 import com.litclub.Backend.entity.ClubMembership;
+import com.litclub.Backend.entity.User;
 import com.litclub.Backend.entity.compositeKey.ClubMembershipID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 
 public interface ClubMembershipRepository extends JpaRepository<ClubMembership, ClubMembershipID> {
+
+    List<ClubMembership> findByClub(Club club);
+    List<ClubMembership> findByMember(User user);
+    Optional<ClubMembership> findByClubAndUser(Club club, User user);
+    Optional<ClubMembership> findClubMembershipByClubMembershipID(ClubMembershipID clubMembershipID);
 }
