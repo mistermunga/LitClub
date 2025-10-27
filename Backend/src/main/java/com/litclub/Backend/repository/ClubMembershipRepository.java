d65d6da9532a842b5d68e4a03253c072f08c9c2e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-27 21:23:13 +0300

    made it into a proper JPA Repository

--- Backend/src/main/java/com/litclub/Backend/repository/ClubMembershipRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/ClubMembershipRepository.java
@@ -1,4 +1,9 @@
 package com.litclub.Backend.repository;
 
-public interface ClubMembershipRepository {
+import com.litclub.Backend.entity.ClubMembership;
+import com.litclub.Backend.entity.compositeKey.ClubMembershipID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ClubMembershipRepository extends JpaRepository<ClubMembership, ClubMembershipID> {
 }
