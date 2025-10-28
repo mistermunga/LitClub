2506108b0d91abf0f8270fd6a5b4d8a5ac1bc7de
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-28 12:39:47 +0300

    implemented queries

--- Backend/src/main/java/com/litclub/Backend/repository/UserBooksRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/UserBooksRepository.java
@@ -1,8 +1,19 @@
 package com.litclub.Backend.repository;
 
-import com.litclub.Backend.entity.UserBooks;
-import com.litclub.Backend.entity.compositeKey.UserBooksID;
+import com.litclub.Backend.entity.Book;
+import com.litclub.Backend.entity.User;
+import com.litclub.Backend.entity.UserBook;
+import com.litclub.Backend.entity.compositeKey.UserBookID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserBooksRepository extends JpaRepository<UserBooks, UserBooksID> {
+import java.util.List;
+import java.util.Optional;
+
+public interface UserBooksRepository extends JpaRepository<UserBook, UserBookID> {
+
+    Optional<UserBook> findUserBooksByUserAndBook(User user, Book book);
+    Optional<UserBook> findUserBooksByUserBookID(UserBookID userBookID);
+
+    List<UserBook> findAllByBook(Book book);
+    List<UserBook> findAllByUser(User user);
 }
