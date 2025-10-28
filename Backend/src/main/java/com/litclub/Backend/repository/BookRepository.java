0c4a056dc125d53c025eac7d29be09128d8deed5
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-28 23:55:03 +0300

    queries

--- Backend/src/main/java/com/litclub/Backend/repository/BookRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/BookRepository.java
@@ -3,5 +3,10 @@ package com.litclub.Backend.repository;
 import com.litclub.Backend.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findBookByisbn(String isbn);
+    Optional<Book> findBookByTitleAndPrimaryAuthor(String title, String author);
 }
