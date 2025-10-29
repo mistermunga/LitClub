9376fbd7afabf7fda52094f0f4dc9c9df10a43e0
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-29 20:31:07 +0300

    added note queries

--- Backend/src/main/java/com/litclub/Backend/repository/NoteRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/NoteRepository.java
@@ -1,7 +1,27 @@
 package com.litclub.Backend.repository;
 
+import com.litclub.Backend.entity.Book;
+import com.litclub.Backend.entity.Club;
 import com.litclub.Backend.entity.Note;
+import com.litclub.Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    boolean existsByUserAndBookAndContent(User user, Book book, String content);
+    boolean existsByUserAndBookAndContentAndClub(User user, Book book, String content, Club club);
+
+    Optional<Note> findNoteByNoteID(long noteID);
+
+    List<Note> findAllByUser(User user);
+    List<Note> findAllByBook(Book book);
+    List<Note> findAllByClub(Club club);
+    List<Note> findAllByUserAndBook(User user, Book book);
+    List<Note> findAllByUserAndClub(User user, Club club);
+    List<Note> findAllByBookAndClub(Book book, Club club);
+    List<Note> findAllByUserAndContentContaining(User user, String content);
+
 }
