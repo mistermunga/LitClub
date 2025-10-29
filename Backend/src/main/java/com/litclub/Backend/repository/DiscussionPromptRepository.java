ebcdf3c1d34ca32fbf5856042dd15a1bcd643eb8
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-29 18:05:59 +0300

    service queries

--- Backend/src/main/java/com/litclub/Backend/repository/DiscussionPromptRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/DiscussionPromptRepository.java
@@ -1,7 +1,21 @@
 package com.litclub.Backend.repository;
 
+import com.litclub.Backend.entity.Club;
 import com.litclub.Backend.entity.DiscussionPrompt;
+import com.litclub.Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DiscussionPromptRepository extends JpaRepository<DiscussionPrompt, Long> {
+
+    boolean existsByPromptAndClub(String prompt, Club club);
+
+    Optional<DiscussionPrompt> findByPromptID(Long promptID);
+    Optional<DiscussionPrompt> findByPromptAndClub(String prompt, Club club);
+    List<DiscussionPrompt> findAllByClub(Club club);
+    List<DiscussionPrompt> findAllByPoster(User poster);
+    List<DiscussionPrompt> findAllByPosterAndClub(User poster, Club club);
+
 }
