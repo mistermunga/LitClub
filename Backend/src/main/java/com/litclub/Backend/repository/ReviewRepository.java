3687afa771d91ba846cbf33a0d6d8a2f49f239f5
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-29 22:34:27 +0300

    added some queries

--- Backend/src/main/java/com/litclub/Backend/repository/ReviewRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/ReviewRepository.java
@@ -23,7 +23,9 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByBookAndContentContainsIgnoreCase(Book book, String content);
     List<Review> findByUserAndContentContainsIgnoreCase(User user, String content);
     List<Review> findByContentContainsIgnoreCase(String content);
-    List<Review> findByRatingGreaterThan(int rating);
-    List<Review> findByRatingLessThan(int rating);
+    List<Review> findByBookAndRatingGreaterThan(Book book, int rating);
+    List<Review> findByUserAndRatingGreaterThan(User user, int rating);
+    List<Review> findByBookAndRatingLessThan(Book book, int rating);
+    List<Review> findByUserAndRatingLessThan(User user, int rating);
 
 }
