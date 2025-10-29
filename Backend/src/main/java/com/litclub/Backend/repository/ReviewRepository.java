780ffb1d665fd88537ee1ae714591cf59e97d742
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-29 22:26:58 +0300

    created queries

--- Backend/src/main/java/com/litclub/Backend/repository/ReviewRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/ReviewRepository.java
@@ -1,7 +1,29 @@
 package com.litclub.Backend.repository;
 
+import com.litclub.Backend.entity.Book;
 import com.litclub.Backend.entity.Review;
+import com.litclub.Backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    boolean existsByUserAndBook(User user, Book book);
+
+    Optional<Review> findByBookAndUser(Book book, User user);
+
+    List<Review> findByUser(User user);
+    List<Review> findByBook(Book book);
+    List<Review> findByRatingEquals(int rating);
+
+    List<Review> findByUserAndRatingEquals(User user, int rating);
+    List<Review> findByBookAndRatingEquals(Book book, int rating);
+    List<Review> findByBookAndContentContainsIgnoreCase(Book book, String content);
+    List<Review> findByUserAndContentContainsIgnoreCase(User user, String content);
+    List<Review> findByContentContainsIgnoreCase(String content);
+    List<Review> findByRatingGreaterThan(int rating);
+    List<Review> findByRatingLessThan(int rating);
+
 }
