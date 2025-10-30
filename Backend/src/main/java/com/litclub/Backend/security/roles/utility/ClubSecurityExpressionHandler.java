5229ab36ed048e3a69d2455c215115afdbf9ce15
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-30 20:37:12 +0300

    created ClubSecurityExpressionHandler

---
+++ Backend/src/main/java/com/litclub/Backend/security/roles/utility/ClubSecurityExpressionHandler.java
@@ -0,0 +1,34 @@
+package com.litclub.Backend.security.roles.utility;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+/**
+ * A security class for handling club-level authentication
+ *
+ * <p>This class utilises Method Security to secure the methods
+ * in the top level gatekeeper Services</p>
+ */
+@Component("clubSecurity")
+public class ClubSecurityExpressionHandler {
+
+    public boolean isOwner(Authentication auth, Long clubID) {
+        return hasAuthority(auth, "CLUB_" + clubID + "_OWNER");
+    }
+
+    public boolean isModerator(Authentication auth, Long clubID) {
+        return hasAuthority(auth, "CLUB_" + clubID + "_MODERATOR");
+    }
+
+    public boolean isMember(Authentication auth, Long clubID) {
+        return hasAuthority(auth, "CLUB_" + clubID + "_MEMBER");
+    }
+
+    private boolean hasAuthority(Authentication auth, String role) {
+        if (auth == null || auth.getAuthorities() == null) return false;
+        return auth.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .allMatch(a -> a.equals(role));
+    }
+}
