aba719255c76fbe54f29ab0e4d0825fc854e7beb
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-31 20:12:04 +0300

    new DTO for books

---
+++ Backend/src/main/java/com/litclub/Backend/construct/book/BookDTO.java
@@ -0,0 +1,16 @@
+package com.litclub.Backend.construct.book;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public record BookDTO(
+        Long bookID,
+        String title,
+        List<String> authors,
+        String isbn,
+        String coverUrl,
+        String publisher,
+        LocalDate year,
+        BookStatus status
+) {
+}
