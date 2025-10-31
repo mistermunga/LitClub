b15db08daa64303bba0a4da96c2601a6fd9cf7ec
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-31 12:15:48 +0300

    added filter queries

--- Backend/src/main/java/com/litclub/Backend/repository/ClubMembershipRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/ClubMembershipRepository.java
@@ -4,16 +4,20 @@ import com.litclub.Backend.entity.Club;
 import com.litclub.Backend.entity.ClubMembership;
 import com.litclub.Backend.entity.User;
 import com.litclub.Backend.entity.compositeKey.ClubMembershipID;
+import com.litclub.Backend.security.roles.ClubRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 public interface ClubMembershipRepository extends JpaRepository<ClubMembership, ClubMembershipID> {
 
     List<ClubMembership> findByClub(Club club);
     List<ClubMembership> findByMember(User user);
+    List<ClubMembership> findClubMembershipsByRoles(Set<ClubRole> roles);
+    List<ClubMembership> findClubMembershipsByClubAndRoles(Club club, Set<ClubRole> role);
     Optional<ClubMembership> findByClubAndMember(Club club, User user);
     Optional<ClubMembership> findClubMembershipByClubMembershipID(ClubMembershipID clubMembershipID);
 }
