66ea850ab4491587ab5eae048c3683c638d7fe66
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-10-31 22:39:52 +0300

    minor adjustments to the DTO

--- Backend/src/main/java/com/litclub/Backend/construct/club/ClubRecord.java
+++ Backend/src/main/java/com/litclub/Backend/construct/club/ClubRecord.java
@@ -2,12 +2,13 @@ package com.litclub.Backend.construct.club;
 
 import com.litclub.Backend.construct.user.UserRecord;
 
+import java.util.List;
 import java.util.Set;
 
 public record ClubRecord(
         Long clubID,
         String name,
-        UserRecord administrator,
+        List<UserRecord> administrators,
         Set<UserRecord> members
 ) {
 }
