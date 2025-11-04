15374e2f091db22be1efa95b5752bad3999c45b4
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-04 11:59:12 +0300

    added reviews

--- Backend/src/main/java/com/litclub/Backend/controller/book/BookController.java
+++ Backend/src/main/java/com/litclub/Backend/controller/book/BookController.java
@@ -1,18 +1,24 @@
 package com.litclub.Backend.controller.book;
 
 import com.litclub.Backend.construct.library.BookAddRequest;
+import com.litclub.Backend.construct.library.ReviewRequest;
 import com.litclub.Backend.construct.library.book.BookSearchRequest;
+import com.litclub.Backend.construct.review.ReviewDTO;
 import com.litclub.Backend.construct.user.UserRecord;
 import com.litclub.Backend.entity.Book;
 import com.litclub.Backend.entity.Review;
 import com.litclub.Backend.entity.User;
+import com.litclub.Backend.exception.MalformedDTOException;
+import com.litclub.Backend.security.userdetails.CustomUserDetails;
 import com.litclub.Backend.service.low.ReviewService;
 import com.litclub.Backend.service.middle.BookService;
 import com.litclub.Backend.service.middle.UserService;
+import com.litclub.Backend.service.top.facilitator.LibraryManagementService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
 
@@ -25,13 +31,15 @@ public class BookController {
 
     private final BookService bookService;
     private final ReviewService reviewService;
+    private final LibraryManagementService libraryManagementService;
 
     public BookController(
             BookService bookService,
-            ReviewService reviewService
-    ) {
+            ReviewService reviewService,
+            LibraryManagementService libraryManagementService) {
         this.bookService = bookService;
         this.reviewService = reviewService;
+        this.libraryManagementService = libraryManagementService;
     }
 
     @GetMapping
@@ -70,8 +78,53 @@ public class BookController {
     // ====== Reviews ======
     @GetMapping("/{bookID}/reviews")
     @PreAuthorize("hasRole('USER')")
-    public ResponseEntity<List<Review>> getBookReviews(@PathVariable("bookID") Long bookID) {
-        return ResponseEntity.ok(reviewService.getReviews(bookService.getBook(bookID)));
+    public ResponseEntity<Page<Review>> getBookReviews(@PathVariable("bookID") Long bookID, Pageable pageable) {
+        return ResponseEntity.ok(reviewService.getReviews(bookService.getBook(bookID), pageable));
+    }
+
+    @PostMapping("/{bookID}/reviews")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Review> postReview(
+            @PathVariable("bookID") Long bookID,
+            @RequestBody ReviewRequest reviewRequest,
+            @AuthenticationPrincipal CustomUserDetails cud
+            ) {
+        return ResponseEntity.ok(
+                libraryManagementService.rateAndReviewBook(cud.getUserID(), bookID, reviewRequest)
+        );
+    }
+
+    @PutMapping("/{bookID}/reviews/{reviewID}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Review> updateReview(
+            @PathVariable Long bookID,
+            @PathVariable Long reviewID,
+            @RequestBody ReviewDTO review,
+            @AuthenticationPrincipal CustomUserDetails cud
+    ) {
+        if (!bookID.equals(review.getBookID())) {
+            throw new MalformedDTOException("bookID mismatch");
+        }
+        if (!cud.getUserID().equals(review.getUserID())) {
+            throw new MalformedDTOException("userID mismatch");
+        }
+        return ResponseEntity.ok(
+                reviewService.updateReview(review, reviewID)
+        );
+    }
+
+    @DeleteMapping("/{bookID}/reviews/{reviewID}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Void> deleteReview(
+            @PathVariable Long bookID,
+            @PathVariable Long reviewID
+    ) {
+        Review review = reviewService.getReview(reviewID);
+        if (!review.getBook().getBookID().equals(bookID)) {
+            throw new MalformedDTOException("bookID mismatch");
+        }
+        reviewService.deleteReview(review);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{bookID}/reviews/average")
