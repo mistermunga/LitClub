e45333d38fe405641f1701abafcc5de4f2f4680e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-04 14:05:19 +0300

    notes CRUD

--- Backend/src/main/java/com/litclub/Backend/controller/book/BookController.java
+++ Backend/src/main/java/com/litclub/Backend/controller/book/BookController.java
@@ -3,13 +3,17 @@ package com.litclub.Backend.controller.book;
 import com.litclub.Backend.construct.library.BookAddRequest;
 import com.litclub.Backend.construct.library.ReviewRequest;
 import com.litclub.Backend.construct.library.book.BookSearchRequest;
+import com.litclub.Backend.construct.note.NoteCreateRequest;
 import com.litclub.Backend.construct.review.ReviewDTO;
 import com.litclub.Backend.construct.user.UserRecord;
 import com.litclub.Backend.entity.Book;
+import com.litclub.Backend.entity.Note;
 import com.litclub.Backend.entity.Review;
 import com.litclub.Backend.entity.User;
 import com.litclub.Backend.exception.MalformedDTOException;
+import com.litclub.Backend.security.roles.GlobalRole;
 import com.litclub.Backend.security.userdetails.CustomUserDetails;
+import com.litclub.Backend.service.low.NoteService;
 import com.litclub.Backend.service.low.ReviewService;
 import com.litclub.Backend.service.middle.BookService;
 import com.litclub.Backend.service.middle.UserService;
@@ -17,6 +21,7 @@ import com.litclub.Backend.service.top.facilitator.LibraryManagementService;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -24,6 +29,7 @@ import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/books")
@@ -32,14 +38,16 @@ public class BookController {
     private final BookService bookService;
     private final ReviewService reviewService;
     private final LibraryManagementService libraryManagementService;
+    private final NoteService noteService;
 
     public BookController(
             BookService bookService,
             ReviewService reviewService,
-            LibraryManagementService libraryManagementService) {
+            LibraryManagementService libraryManagementService, NoteService noteService) {
         this.bookService = bookService;
         this.reviewService = reviewService;
         this.libraryManagementService = libraryManagementService;
+        this.noteService = noteService;
     }
 
     @GetMapping
@@ -145,4 +153,98 @@ public class BookController {
         return ResponseEntity.ok(page);
     }
 
+
+    // ====== NOTES ======
+    @GetMapping("/{bookID}/notes")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Page<Note>> getNotes(
+            Pageable pageable,
+            @PathVariable("bookID") Long bookID
+    ) {
+        Book book = bookService.getBook(bookID);
+        return ResponseEntity.ok(
+                noteService.getAllNotes(book, pageable)
+        );
+    }
+
+    @PostMapping("/{bookID}/notes")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Note> postNote( // non club notes
+            @PathVariable("bookID") Long bookID,
+            @RequestBody NoteCreateRequest ncr,
+            @AuthenticationPrincipal CustomUserDetails cud
+    ) {
+        if (!bookID.equals(ncr.bookID())){
+            throw new MalformedDTOException("bookID mismatch");
+        }
+        Book book = bookService.getBook(bookID);
+        User user = cud.getUser();
+        return ResponseEntity.ok(
+                noteService.save(
+                        user,
+                        book,
+                        ncr.content(),
+                        Optional.empty(),
+                        Optional.empty(),
+                        ncr.isPrivate()
+                )
+        );
+    }
+
+    @PutMapping("/{bookID}/notes/{noteID}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Note> updateNote(
+            @PathVariable("bookID") Long bookID,
+            @PathVariable("noteID") Long noteID,
+            @RequestBody String content,
+            @AuthenticationPrincipal CustomUserDetails cud
+    ) {
+        Note note = noteService.getNoteById(noteID);
+        if (!note.getBook().getBookID().equals(bookID)) {
+            throw new MalformedDTOException("bookID mismatch");
+        }
+        if (!note.getUser().equals(cud.getUser())) {
+            throw new MalformedDTOException("note does not belong to user");
+        }
+        return ResponseEntity.ok(
+                noteService.updateNote(content, noteID)
+        );
+    }
+
+    @GetMapping("/{bookID}/notes/{noteID}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Note> getNote(
+            @PathVariable("bookID") Long bookID,
+            @PathVariable("noteID") Long noteID,
+            @AuthenticationPrincipal CustomUserDetails cud
+    ) {
+        Note note = noteService.getNoteById(noteID);
+        if (!note.getBook().getBookID().equals(bookID)) {
+            throw new MalformedDTOException("bookID mismatch");
+        }
+        if (note.isPrivate()) {
+            if (!note.getUser().equals(cud.getUser()) && !cud.getAuthorities().contains(GlobalRole.ADMINISTRATOR)) {
+                throw new AccessDeniedException("note is private");
+            }
+        }
+        return ResponseEntity.ok(note);
+    }
+
+    @DeleteMapping("/{bookID}/notes/{noteID}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<Void> deleteNote(
+            @PathVariable("bookID") Long bookID,
+            @PathVariable("noteID") Long noteID,
+            @AuthenticationPrincipal CustomUserDetails cud
+    ) {
+        Note note = noteService.getNoteById(noteID);
+        if (!note.getBook().getBookID().equals(bookID)) {
+            throw new MalformedDTOException("bookID mismatch");
+        }
+        if (!note.getUser().equals(cud.getUser()) && !cud.getAuthorities().contains(GlobalRole.ADMINISTRATOR)) {
+            throw new AccessDeniedException("cannot delete another user's note");
+        }
+        noteService.deleteNote(noteID);
+        return ResponseEntity.ok().build();
+    }
 }
