79f599aabfad51d65d8557a1a678843e1c236a1d
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-05 20:18:35 +0300

    added new dependencies for web

--- Desktop/src/main/java/module-info.java
+++ Desktop/src/main/java/module-info.java
@@ -5,11 +5,16 @@ module com.litclub {
     requires java.desktop;
     requires com.google.gson;
     requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires java.net.http;
 
     opens com.litclub to javafx.fxml, com.google.gson, com.fasterxml.jackson.databind;
+    opens com.litclub.client.api to com.fasterxml.jackson.databind, com.google.gson, javafx.fxml, com.fasterxml.jackson.datatype.jsr310;
     opens com.litclub.persistence to com.google.gson;
-    opens com.litclub.session.construct to com.google.gson;
-    opens com.litclub.session.construct.mock to com.google.gson;
+    opens com.litclub.construct to com.google.gson;
+    opens com.litclub.construct.mock to com.google.gson;
 
     exports com.litclub;
+    exports com.litclub.client.api;
+    opens com.litclub.construct.record.user to com.google.gson;
 }
