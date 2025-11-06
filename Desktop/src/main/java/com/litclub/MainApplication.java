c1bf9ed043a5bf8be63637d31b632206a3b11dae
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-06 15:20:05 +0300

    included SceneManager

--- Desktop/src/main/java/com/litclub/MainApplication.java
+++ Desktop/src/main/java/com/litclub/MainApplication.java
@@ -1,17 +1,41 @@
 package com.litclub;
 
+import com.litclub.theme.SceneManager;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
+import java.util.Objects;
 
 public class MainApplication extends Application {
 
+    private static MainApplication instance;
+    private Stage primaryStage;
+
+    public static MainApplication getInstance() {
+        return instance;
+    }
+
+    static {System.setProperty("file.encoding", "UTF-8");}
+
     @Override
-    public void start(Stage stage) {
-        stage.setScene(new Scene(new Label("LitClub Launcher!"), 300, 200));
-        stage.setTitle("LitClub");
-        stage.show();
+    public void start(Stage primaryStage) {
+        instance = this;
+        this.primaryStage = primaryStage;
+
+        primaryStage.getIcons().add(
+                new Image(Objects.requireNonNull(getClass().getResourceAsStream(
+                        "/com/litclub/ui/icons/LitClub_logo_blackText_mini.png"
+                ))
+                ));
+
+        SceneManager.getInstance().showLanding();
+    }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
     }
 
     public static void main(String[] args) {
