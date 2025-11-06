377716accce377522552e52263196ad1c8eb7bec
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-06 13:48:14 +0300

    configured dependencies

--- Desktop/src/main/java/module-info.java
+++ Desktop/src/main/java/module-info.java
@@ -1,10 +1,15 @@
 module com.litclub {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.desktop;
+    requires com.google.gson;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
+    requires java.net.http;
+    requires com.fasterxml.jackson.annotation;
 
-    // If you later use FXML controllers, this allows FXMLLoader to access them reflectively
     opens com.litclub to javafx.fxml;
 
-    // Export your main package so JavaFX can find MainApplication
     exports com.litclub;
 }
