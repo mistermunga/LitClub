5373e6df04416584254cacc00abc4c83ef6bbabf
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-06 13:39:18 +0300

    initiating main build

--- Desktop/src/main/java/module-info.java
+++ Desktop/src/main/java/module-info.java
@@ -1,21 +1,10 @@
 module com.litclub {
     requires javafx.controls;
     requires javafx.fxml;
-    requires javafx.graphics;
-    requires java.desktop;
-    requires com.google.gson;
-    requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.datatype.jsr310;
-    requires java.net.http;
-    requires com.fasterxml.jackson.annotation;
 
-    opens com.litclub to javafx.fxml, com.google.gson, com.fasterxml.jackson.databind;
-    opens com.litclub.client.api to com.fasterxml.jackson.databind, com.google.gson, javafx.fxml, com.fasterxml.jackson.datatype.jsr310;
-    opens com.litclub.persistence to com.google.gson;
-    opens com.litclub.construct to com.google.gson;
-    opens com.litclub.construct.mock to com.google.gson;
+    // If you later use FXML controllers, this allows FXMLLoader to access them reflectively
+    opens com.litclub to javafx.fxml;
 
+    // Export your main package so JavaFX can find MainApplication
     exports com.litclub;
-    exports com.litclub.client.api;
-    opens com.litclub.construct.record.user to com.google.gson;
 }
