eca2bf272a3a4379d31b2a12f210ef7b27fe3e25
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-07 18:37:07 +0300

    opened packages to Jackson for introspection

--- Desktop/src/main/java/module-info.java
+++ Desktop/src/main/java/module-info.java
@@ -6,11 +6,24 @@ module com.litclub {
     requires com.google.gson;
     requires com.fasterxml.jackson.databind;
     requires com.fasterxml.jackson.datatype.jsr310;
+    requires com.fasterxml.jackson.datatype.jdk8;
     requires java.net.http;
     requires com.fasterxml.jackson.annotation;
     requires org.json;
 
     opens com.litclub to javafx.fxml;
+    opens com.litclub.construct to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.enums to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.compositeKey to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.auth to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.club to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.note to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.user to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.review to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.library to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.meeting to com.fasterxml.jackson.databind, com.google.gson;
+    opens com.litclub.construct.interfaces.discussion to com.fasterxml.jackson.databind, com.google.gson;
 
     exports com.litclub;
 }
