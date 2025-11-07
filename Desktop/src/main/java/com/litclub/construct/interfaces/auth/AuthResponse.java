b13327200567895961b13694604bd3312b48843c
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-07 09:24:35 +0300

    rename

--- Desktop/src/main/java/com/litclub/construct/interfaces/auth/AuthResponse.java
+++ Desktop/src/main/java/com/litclub/construct/interfaces/auth/AuthResponse.java
@@ -2,4 +2,4 @@ package com.litclub.construct.interfaces.auth;
 
 import com.litclub.construct.interfaces.user.UserRecord;
 
-public record AuthResponse(String token, UserRecord userRecord) {}
+public record AuthResponse(String token, UserRecord user) {}
