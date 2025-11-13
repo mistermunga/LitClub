6c3a402bcd6bc9a45c7c27da05751fa47ef0af6e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-13 18:30:52 +0300

    added HeaderBar and its components

---
+++ Desktop/src/main/java/com/litclub/ui/main/shared/HeaderBar.java
@@ -0,0 +1,52 @@
+package com.litclub.ui.main.shared;
+
+import com.litclub.SceneManager;
+import com.litclub.theme.ThemeManager;
+import com.litclub.ui.main.shared.subcomponents.MiniLogo;
+import com.litclub.ui.main.shared.subcomponents.UserMessage;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
+
+public class HeaderBar extends HBox {
+
+    public HeaderBar() {
+        ThemeManager.getInstance().registerComponent(this);
+        this.setAlignment(Pos.CENTER);
+        this.setPadding(new Insets(10, 30, 10, 30));
+        this.setSpacing(12);
+        this.setMaxHeight(60);
+        this.getStyleClass().add("header-bar");
+
+        addLogoSection();
+        addSpacer();
+        addGreeting();
+    }
+
+    private void addLogoSection() {
+        HBox logoSection = new HBox(10);
+        logoSection.setAlignment(Pos.BASELINE_CENTER);
+
+        MiniLogo miniLogo = new MiniLogo();
+        miniLogo.setOnMouseClicked(e -> {
+            SceneManager.getInstance().showCrossRoads();
+        });
+
+        logoSection.getChildren().addAll(miniLogo);
+        this.getChildren().add(logoSection);
+    }
+
+    private void addSpacer() {
+        Region spacer = new Region();
+        HBox.setHgrow(spacer, Priority.ALWAYS);
+        this.getChildren().add(spacer);
+    }
+
+    private void addGreeting() {
+        UserMessage userMessage = new UserMessage();
+        userMessage.setAlignment(Pos.BASELINE_CENTER);
+        this.getChildren().add(userMessage);
+    }
+}
---
+++ Desktop/src/main/java/com/litclub/ui/main/shared/subcomponents/UserMessage.java
@@ -0,0 +1,23 @@
+package com.litclub.ui.main.shared.subcomponents;
+
+import com.litclub.session.AppSession;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class UserMessage extends HBox {
+
+    public UserMessage() {
+        this.setAlignment(Pos.CENTER_RIGHT);
+        this.setSpacing(5);
+
+        Label greeting = new Label("Hello,");
+        greeting.getStyleClass().add("greeting-label");
+
+        Button userButton = new Button(AppSession.getInstance().getUserRecord().username());
+        userButton.getStyleClass().add("user-button");
+
+        this.getChildren().addAll(greeting, userButton);
+    }
+}
