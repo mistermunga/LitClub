df09dcbf28dc0c95aad88c2095f8505c0caf3d99
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-13 15:35:30 +0300

    added JacksonConfig

---
+++ Backend/src/main/java/com/litclub/Backend/config/JacksonConfig.java
@@ -0,0 +1,20 @@
+package com.litclub.Backend.config;
+
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Jdk8Module());
+        return mapper;
+    }
+}
+
