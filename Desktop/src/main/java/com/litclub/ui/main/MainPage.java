3358e494ea2d49c74eb5739b64cdbc618c5c97f8
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-14 14:52:07 +0300

    completed adding content area to main page

--- Desktop/src/main/java/com/litclub/ui/main/MainPage.java
+++ Desktop/src/main/java/com/litclub/ui/main/MainPage.java
@@ -1,7 +1,9 @@
 package com.litclub.ui.main;
 
 import com.litclub.theme.ThemeManager;
+import com.litclub.ui.main.shared.ContentArea;
 import com.litclub.ui.main.shared.HeaderBar;
+import com.litclub.ui.main.shared.NavigationSideBar;
 import javafx.scene.layout.BorderPane;
 
 public class MainPage extends BorderPane {
@@ -13,11 +15,20 @@ public class MainPage extends BorderPane {
         ThemeManager.getInstance().registerComponent(this);
         this.getStyleClass().add("root");
         showHeader();
+        showDashBoard();
     }
 
     private void showHeader() {
         HeaderBar headerBar = new HeaderBar();
         this.setTop(headerBar);
+    }
+
+    private void showDashBoard() {
+        ContentArea contentArea = new ContentArea(isPersonal);
+        NavigationSideBar navBar = new NavigationSideBar(contentArea, isPersonal);
+
+        this.setLeft(navBar);
+        this.setCenter(contentArea);
     }
 
 }
