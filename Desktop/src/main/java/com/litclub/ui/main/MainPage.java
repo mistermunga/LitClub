78271ef03fc8c4af392678d1ef1af46f44889c06
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-14 13:52:22 +0300

    added Header to MainPage

--- Desktop/src/main/java/com/litclub/ui/main/MainPage.java
+++ Desktop/src/main/java/com/litclub/ui/main/MainPage.java
@@ -1,9 +1,23 @@
 package com.litclub.ui.main;
 
-import javafx.scene.layout.StackPane;
+import com.litclub.theme.ThemeManager;
+import com.litclub.ui.main.shared.HeaderBar;
+import javafx.scene.layout.BorderPane;
 
-public class MainPage extends StackPane {
+public class MainPage extends BorderPane {
 
-    public MainPage(boolean isPersonal) {}
+    private final boolean isPersonal;
+
+    public MainPage(boolean isPersonal) {
+        this.isPersonal = isPersonal;
+        ThemeManager.getInstance().registerComponent(this);
+        this.getStyleClass().add("root");
+        showHeader();
+    }
+
+    private void showHeader() {
+        HeaderBar headerBar = new HeaderBar();
+        this.setTop(headerBar);
+    }
 
 }
