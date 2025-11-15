5bf91797129e26301f4c1d2dc5886aa11059794e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-15 22:56:25 +0300

    expanded dto

--- Desktop/src/main/java/com/litclub/construct/interfaces/library/BookWithStatus.java
+++ Desktop/src/main/java/com/litclub/construct/interfaces/library/BookWithStatus.java
@@ -1,16 +1,17 @@
 package com.litclub.construct.interfaces.library;
 
-
-
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.litclub.construct.Book;
 import com.litclub.construct.enums.BookStatus;
 
 import java.time.LocalDate;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record BookWithStatus(
-        Book book,
-        BookStatus status,
-        Integer rating,
-        LocalDate dateStarted,
-        LocalDate dateFinished
+        @JsonProperty("book") Book book,
+        @JsonProperty("status") BookStatus status,
+        @JsonProperty("rating") Integer rating,
+        @JsonProperty("dateStarted") LocalDate dateStarted,
+        @JsonProperty("dateFinished") LocalDate dateFinished
 ) {}
