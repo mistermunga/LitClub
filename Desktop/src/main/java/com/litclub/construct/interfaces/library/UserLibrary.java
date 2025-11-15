769cc89e6e07d7372b5b741be57221a65ffea9ee
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-15 22:54:06 +0300

    Json properties

--- Desktop/src/main/java/com/litclub/construct/interfaces/library/UserLibrary.java
+++ Desktop/src/main/java/com/litclub/construct/interfaces/library/UserLibrary.java
@@ -1,16 +1,18 @@
 package com.litclub.construct.interfaces.library;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.litclub.construct.Review;
 import com.litclub.construct.interfaces.user.UserRecord;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record UserLibrary(
-        UserRecord user,
-        List<BookWithStatus> currentlyReading,
-        List<BookWithStatus> wantToRead,
-        List<BookWithStatus> read,
-        List<BookWithStatus> dnf,
-        List<Review> recentReviews
+        @JsonProperty("user") UserRecord user,
+        @JsonProperty("currentlyReading") List<BookWithStatus> currentlyReading,
+        @JsonProperty("wantToRead") List<BookWithStatus> wantToRead,
+        @JsonProperty("read") List<BookWithStatus> read,
+        @JsonProperty("dnf") List<BookWithStatus> dnf,
+        @JsonProperty("recentReviews") List<Review> recentReviews
 ) {}
-
