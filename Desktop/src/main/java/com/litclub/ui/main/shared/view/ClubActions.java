6f3bebdb2c05d828ed81536bc2a3c6263391b93f
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-18 14:56:43 +0300

    added invite generation

--- Desktop/src/main/java/com/litclub/ui/main/shared/view/ClubActions.java
+++ Desktop/src/main/java/com/litclub/ui/main/shared/view/ClubActions.java
@@ -2,35 +2,69 @@ package com.litclub.ui.main.shared.view;
 
 import com.litclub.construct.enums.ClubRole;
 import com.litclub.session.AppSession;
+import com.litclub.ui.main.shared.view.service.ClubService;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.VBox;
 
 public class ClubActions extends VBox {
 
+    private final ClubService clubService;
+
     public ClubActions() {
         this.setSpacing(12);
         this.getStyleClass().add("club-actions");
+        this.clubService = new ClubService();
 
         ClubRole highestRole = AppSession.getInstance().getHighestRole();
+        Node content = switch (highestRole) {
+            case MODERATOR -> createModeratorUI();
+            case OWNER -> createOwnerUI();
+            default -> createDefaultUI();
+        };
 
-        switch (highestRole) {
-            case MODERATOR -> initModeratorUI();
-            case OWNER -> initOwnerUI();
-            default -> initDefaultUI();
-        }
+        // Wrap UI inside a container Node
+        TitledPane container = new TitledPane("Club Actions", content);
+        container.setCollapsible(false);
+
+        getChildren().add(container);
     }
 
-    private void initModeratorUI() {
-        getChildren().add(new Label("Moderator Tools"));
+    private Node createModeratorUI() {
+        VBox box = new VBox(8);
+        box.getChildren().add(new Label("Moderator Tools"));
+
+        Button getInviteButton = new Button("Generate Invite");
+        Label codeLabel = new Label();
+
+        getInviteButton.setOnAction(e -> {
+            String code = clubService.generateInvite();
+            codeLabel.setText(code);
+            System.out.println("Generated Invite: " + code);
+        });
+
+        box.getChildren().addAll(getInviteButton, codeLabel);
+
         // TODO add moderator-specific buttons, etc
+
+        return box;
     }
 
-    private void initOwnerUI() {
-        getChildren().add(new Label("Owner Tools"));
+    private Node createOwnerUI() {
+        VBox box = new VBox(10);
+
+        // Reuse moderator UI inside owner UI
+        box.getChildren().add(createModeratorUI());
+
+        box.getChildren().add(new Label("Owner Tools"));
         // TODO add owner-specific controls
+
+        return box;
     }
 
-    private void initDefaultUI() {
-        getChildren().add(new Label("No actions available"));
+    private Node createDefaultUI() {
+        return new Label("No actions available");
     }
 }
