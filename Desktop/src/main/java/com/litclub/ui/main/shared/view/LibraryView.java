da7414566c9abd267970b314517ae4cac7b5fd5e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-20 15:56:37 +0300

    added event bus integration

--- Desktop/src/main/java/com/litclub/ui/main/shared/view/LibraryView.java
+++ Desktop/src/main/java/com/litclub/ui/main/shared/view/LibraryView.java
@@ -1,9 +1,13 @@
 package com.litclub.ui.main.shared.view;
 
 import com.litclub.theme.ThemeManager;
+import com.litclub.ui.main.shared.event.EventBus;
+import com.litclub.ui.main.shared.event.EventBus.EventType;
 import com.litclub.ui.main.shared.view.subcomponent.library.LibraryControlBar;
 import com.litclub.ui.main.shared.view.subcomponent.library.LibraryCore;
 import javafx.scene.layout.BorderPane;
+
+import java.util.Set;
 
 public class LibraryView extends BorderPane {
 
@@ -25,6 +29,15 @@ public class LibraryView extends BorderPane {
 
         this.setTop(controlBar);
         this.setCenter(libraryCore);
+
+        Set<EventType> relevantEvents = Set.of(
+                EventType.BOOKS_UPDATED,
+                EventType.PERSONAL_LIBRARY_UPDATED
+        );
+
+        for (EventType eventType : relevantEvents) {
+            EventBus.getInstance().on(eventType, this::refresh);
+        }
     }
 
     /**
