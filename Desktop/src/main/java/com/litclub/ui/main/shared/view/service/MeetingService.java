ac5aeebdb77396e67aedcf049477f3315c25484e
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-11-24 20:25:02 +0300

    created meeting service

--- Desktop/src/main/java/com/litclub/ui/main/shared/view/service/MeetingService.java
+++ Desktop/src/main/java/com/litclub/ui/main/shared/view/service/MeetingService.java
@@ -1,9 +1,14 @@
 package com.litclub.ui.main.shared.view.service;
 
+import com.litclub.client.api.ApiErrorHandler;
 import com.litclub.construct.Meeting;
+import com.litclub.construct.interfaces.meeting.MeetingCreateRequest;
 import com.litclub.persistence.repository.ClubRepository;
 import com.litclub.session.AppSession;
+import javafx.application.Platform;
 import javafx.collections.ObservableList;
+
+import java.util.function.Consumer;
 
 public class MeetingService {
 
@@ -14,13 +19,37 @@ public class MeetingService {
     public MeetingService() {
         this.clubRepository = ClubRepository.getInstance();
         this.session = AppSession.getInstance();
-
-        clubRepository.fetchClubMeetings(session.getCurrentClub().getClubID());
-
+        loadClubMeetings();
         this.clubMeetings = clubRepository.getMeetings();
+    }
+
+    public void loadClubMeetings() {
+        clubRepository.fetchClubMeetings(session.getCurrentClub().getClubID());
     }
 
     public ObservableList<Meeting> getClubMeetings() {
         return clubMeetings;
+    }
+
+    public void addMeeting(MeetingCreateRequest createRequest,
+                           Consumer<Meeting> onSuccess,
+                           Consumer<String> onError) {
+        clubRepository.createMeeting(
+                session.getCurrentClub().getClubID(),
+                createRequest
+        ).thenAccept(meeting -> {
+            Platform.runLater(() -> {
+                System.out.println("Meeting added");
+                loadClubMeetings();
+                onSuccess.accept(meeting);
+            });
+        }).exceptionally(throwable -> {
+            Platform.runLater(() -> {
+                String error = ApiErrorHandler.parseError(throwable);
+                System.out.println("Meeting adding failed: " + error);
+                onError.accept("Meeting adding failed: " + error);
+            });
+            return null;
+        });
     }
 }
