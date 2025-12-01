58c56454ec2e702e6fad1bebd74fd584c49b6a30
Ryan Munga <131756414+mistermunga@users.noreply.github.com>
2025-12-01 10:55:05 +0300

    added recommendation query

--- Backend/src/main/java/com/litclub/Backend/repository/UserBooksRepository.java
+++ Backend/src/main/java/com/litclub/Backend/repository/UserBooksRepository.java
@@ -4,7 +4,10 @@ import com.litclub.Backend.entity.Book;
 import com.litclub.Backend.entity.User;
 import com.litclub.Backend.entity.UserBook;
 import com.litclub.Backend.entity.compositeKey.UserBookID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,4 +19,19 @@ public interface UserBooksRepository extends JpaRepository<UserBook, UserBookID>
 
     List<UserBook> findAllByBook(Book book);
     List<UserBook> findAllByUser(User user);
+
+    @Query("""
+    SELECT ub.book
+    FROM UserBook ub
+    JOIN ClubMembership cm ON cm.member = ub.user
+    WHERE cm.club IN (
+        SELECT cm2.club
+        FROM ClubMembership cm2
+        WHERE cm2.member = :user
+    )
+    GROUP BY ub.book
+    ORDER BY COUNT(ub.book) DESC
+    """)
+    Page<Book> findRecommendedBooks(User user, Pageable pageable);
+
 }
